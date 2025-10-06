e133589d9cdcad2107c17d7fb4fd16f1ac05ac2e
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2025-10-06 17:21:28 +0530

    folder

--- server/models/transaction/load_out.js
+++ server/models/transaction/load_out.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-const load_out = new mongoose.Schema({
-    
-})
+const load_outSchema = new mongoose.Schema({
+    salesman_code: { type: String, required: true },
+    Date: { type: Date, required: true },
+    item_code: { type: String, required: true },
+    qty: { type: Number, required: true }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Transation_LoadOut', load_outSchema);
