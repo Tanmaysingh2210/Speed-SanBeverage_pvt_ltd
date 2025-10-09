cb77f7f08c974be98ecf1db77dec019d275b26f6
Tanmaysingh2210 <tanmayrajpoot2210@gmail.com>
2025-10-09 21:07:22 +0530

    forget password page done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { ForgetPass } from './pages/ForgetPass';
 //import { RegisterPage } from './pages/RegisterPage';
 import { SignInPage } from './pages/SignInPage';
 //import {SignInPage} from './pages/SignInPage';
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <>
-    <SignInPage />
+      <ForgetPass />
     </>
   )
 }
