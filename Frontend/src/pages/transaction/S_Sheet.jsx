20660c6172e20a075e1d7515c8866937335794bc
Nihal <chaudharynihal299@gmail.com>
2025-11-11 20:56:45 +0530

    s.sheet complete

--- Frontend/src/pages/transaction/S_Sheet.jsx
+++ Frontend/src/pages/transaction/S_Sheet.jsx
@@ -57,6 +57,112 @@ const S_Sheet = () => {
               </div>
             </div>
           </form>
+        
+        <div className="item-inputs gap3">
+            <div className="gap1">
+            <div className="flex">
+              <div className="form-group">
+                <label>Sale</label>
+                <input 
+                   type="number"
+                   placeholder="Enter Sale Price"
+                />
+              </div>
+              <div className="form-group">
+                <label>DEP/REF</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+               <div className="form-group">
+                <label>TOTAL A</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+            </div>
+            <div className="flex">
+               <div className="form-group">
+                <label>SMP,DSC,INCM,SCME</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+
+              <div className="form-group">
+                <label>OTHERS</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+
+
+              <div className="form-group">
+                <label>TOTAL B</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+            </div>
+        </div>  
+
+          <div className="gap2">
+            <div className="flex">
+             <div className="form-group">
+                <label>Cash Sale</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+              
+              <div className="form-group">
+                <label>Net Collection</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+              <div className="form-group">
+                <label>Cheq.Desposited</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+            </div> 
+            <div className="flex">
+              <div className="form-group">
+                <label>Cash Short</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+              <div className="form-group">
+                <label>Short/Excess</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+              <div className="form-group">
+                <label>Cash Deposited</label>
+                <input 
+                   type="number"
+                   placeholder="Enter"
+                />
+              </div>
+
+            </div>
+          </div>
+
+          </div>
         </div>
       </div>
     </div >
