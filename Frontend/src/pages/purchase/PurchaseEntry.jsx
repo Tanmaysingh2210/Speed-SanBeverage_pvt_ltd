d1f52d77a6be0bae119a07b7b9d0a2312c79fd6b
Nihal <chaudharynihal299@gmail.com>
2025-11-15 19:48:42 +0530

    css file error resolve

--- Frontend/src/pages/purchase/PurchaseEntry.jsx
+++ Frontend/src/pages/purchase/PurchaseEntry.jsx
@@ -1,32 +1,67 @@
 import React from 'react';
 import "../transaction/transaction.css";
 
-const PurchaseEntry = ()=>{
+const PurchaseEntry = () => {
 
 
-    return(
+    return (
         <div className="trans">
-            <div className="trans-Container">
+            <div className="trans-container">
                 <div className="trans-left">
                     <form className="trans-form">
                         <div className="salesman-detail">
                             <div className="form-group">
                                 <label>Party</label>
-                                <input 
+                                <input
                                     type="text"
                                     placeholder="enter party no."
                                 />
+                            </div>
 
+                            <div className="form-group">
+                                <label>Slno.</label>
+                                <input
+                                    type="number"
+                                    placeholder="enter slno."
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>Date</label>
+                                <input
+                                    type="date"
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label>GRA</label>
+                                <input
+                                    type="number"
+                                    placeholder="Enter GRA no."
+                                />
                             </div>
                         </div>
                     </form>
 
                 </div>
+                <div className="item-inputs">
+                    <div></div>
+                    <div className="flex">
+                        <div className="form-group">
+                            <label>Sale</label>
+                            <input
+                                type="number"
+                                placeholder="Enter Sale Price"
+                            />
+                        </div>
 
+                    </div>
+
+                </div>
 
 
             </div>
-        
+
         </div>
     )
 }
