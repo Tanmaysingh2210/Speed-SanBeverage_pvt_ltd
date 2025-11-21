acb29531fe50326c4426c6e651de18fd3ec0afa5
shlok-bhatia <shlokbhatia16@gmail.com>
2025-11-21 18:41:51 +0530

    PurchaseNavigations

--- Frontend/src/pages/purchase/PurchaseEntry.jsx
+++ Frontend/src/pages/purchase/PurchaseEntry.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useRef } from 'react';
 import "../purchase/purchase.css"; // New separate CSS file
 import { usePurchase } from '../../context/PurchaseContext.jsx';
 
@@ -25,6 +25,24 @@ const PurchaseEntry = () => {
     });
 
     const [netAmt, setNetAmt] = useState(0);
+
+        const modalPartyRef = useRef(null);
+        const modalSlnoRef = useRef(null);
+        const modalDateRef = useRef(null);
+        const modalGraRef = useRef(null);
+        const modalNameaddRef = useRef(null);
+        const modalVehiclenoRef = useRef(null);
+        const modalVnodtRef = useRef(null);
+        const modalVnoRef = useRef(null);
+        const modalBillRef = useRef(null);
+        const modalErcRef = useRef(null);
+        const modalFrcRef = useRef(null);
+        const modalValueRef = useRef(null);
+        const modalDiscRef = useRef(null);
+        const modalPervatRef = useRef(null);
+        const modalPurchaseagstRef = useRef(null);
+        const modalFormissueRef = useRef(null);
+        const SubmitRef = useRef(null);
 
     // Calculate Net Amount
     useEffect(() => {
@@ -76,6 +94,130 @@ const PurchaseEntry = () => {
         }
     };
 
+    const handleKeyNav = (e, currentField) => {
+        if (["ArrowRight", "ArrowDown", "Enter"].includes(e.key)) {
+            e.preventDefault();
+
+            if (e.key === "Enter" && currentField === "submit") {
+                SubmitRef.current?.click();
+                return;
+            }
+
+            switch (currentField) {
+                case "party":
+                    modalSlnoRef.current?.focus();
+                    break;
+                case "slno":
+                    modalDateRef.current?.focus();
+                    break;
+                case "date":
+                    modalGraRef.current?.focus();
+                    break;
+                case "gra":
+                    modalNameaddRef.current?.focus();
+                    break;
+                case "nameandaddress":
+                    modalVehiclenoRef.current?.focus();
+                    break;
+                case "vehicleno":
+                    modalBillRef.current?.focus();
+                    break;
+                case "nameandaddress":
+                    modalVehiclenoRef.current?.focus();
+                    break;
+                case "bill":
+                    modalVnodtRef.current?.focus();
+                    break;
+                case "vnodt":
+                    modalErcRef.current?.focus();
+                    break;
+                case "erc":
+                    modalVnoRef.current?.focus();
+                    break;
+                case "vno":
+                    modalFrcRef.current?.focus();
+                    break;
+                case "frc":
+                    modalValueRef.current?.focus();
+                    break;
+                case "value":
+                    modalPervatRef.current?.focus();
+                    break;
+                case "pervat":
+                    modalDiscRef.current?.focus();
+                    break;
+                case "disc":
+                    modalPurchaseagstRef.current?.focus();
+                    break;
+                case "purchaseagst":
+                    modalFormissueRef.current?.focus();
+                    break;
+                case "formissue":
+                    if (e.key === "Enter") {
+                        SubmitRef.current?.click();
+                    } else {
+                        SubmitRef.current?.focus();
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        } else if (["ArrowUp", "ArrowLeft"].includes(e.key)) {
+            e.preventDefault();
+            switch (currentField) {
+                case "slno":
+                    modalPartyRef.current?.focus();
+                    break;
+                case "date":
+                    modalSlnoRef.current?.focus();
+                    break;
+                case "gra":
+                    modalDateRef.current?.focus();
+                    break;
+                case "nameandaddress":
+                    modalGraRef.current?.focus();
+                    break;
+                case "vehicleno":
+                    modalNameaddRef.current?.focus();
+                    break;
+                case "bill":
+                    modalVehiclenoRef.current?.focus();
+                    break;
+                case "vnodt":
+                    modalBillRef.current?.focus();
+                    break;
+                case "erc":
+                    modalVnodtRef.current?.focus();
+                    break;
+                case "vno":
+                    modalErcRef.current?.focus();
+                    break;
+                case "frc":
+                    modalVnoRef.current?.focus();
+                    break;
+                case "value":
+                    modalFrcRef.current?.focus();
+                    break;
+                case "pervat":
+                    modalValueRef.current?.focus();
+                    break;
+                case "disc":
+                    modalPervatRef.current?.focus();
+                    break;
+                case "purchaseagst":
+                    modalDiscRef.current?.focus();
+                    break;
+                case "formissue":
+                    modalPurchaseagstRef.current?.focus();
+                    break;
+                case "submit":
+                    modalFormissueRef.current?.focus();
+                    break;
+            }
+        }
+    }
+
     return (
         <div className="purchase-entry-wrapper">
             <div className="purchase-entry-container">
@@ -90,6 +232,8 @@ const PurchaseEntry = () => {
                                 value={formData.party}
                                 onChange={handleChange}
                                 placeholder="enter party"
+                                ref={modalPartyRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "party")}
                             />
                         </div>
 
@@ -101,6 +245,8 @@ const PurchaseEntry = () => {
                                 value={formData.slno}
                                 onChange={handleChange}
                                 placeholder="enter slno."
+                               ref={modalSlnoRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "slno")}
                             />
                         </div>
 
@@ -112,6 +258,8 @@ const PurchaseEntry = () => {
                                 value={formData.date}
                                 onChange={handleChange}
                                 data-placeholder="dd/mm/yyyy"
+                               ref={modalDateRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "date")}
                             />
                         </div>
 
@@ -123,6 +271,8 @@ const PurchaseEntry = () => {
                                 value={formData.gra}
                                 onChange={handleChange}
                                 placeholder="Enter GRA no."
+                                ref={modalGraRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "gra")}
                             />
                         </div>
                     </div>
@@ -137,6 +287,8 @@ const PurchaseEntry = () => {
                                     value={formData.nameAddress}
                                     onChange={handleChange}
                                     placeholder="Enter Name and Address"
+                                    ref={modalNameaddRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "nameandaddress")}
                                 />
                             </div>
                         </div>
@@ -150,6 +302,8 @@ const PurchaseEntry = () => {
                                     value={formData.vehicleNo}
                                     onChange={handleChange}
                                     placeholder="Enter vehicle no."
+                                    ref={modalVehiclenoRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "vehicleno")}
                                 />
                             </div>
 
@@ -161,6 +315,8 @@ const PurchaseEntry = () => {
                                     value={formData.vnoDt}
                                     onChange={handleChange}
                                     placeholder="dd/mm/yyyy"
+                                    ref={modalVnodtRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "vnodt")}
                                 />
                             </div>
 
@@ -172,6 +328,8 @@ const PurchaseEntry = () => {
                                     value={formData.vno}
                                     onChange={handleChange}
                                     placeholder="Enter"
+                                    ref={modalVnoRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "vno")}
                                 />
                             </div>
                         </div>
@@ -185,6 +343,8 @@ const PurchaseEntry = () => {
                                     value={formData.bill}
                                     onChange={handleChange}
                                     placeholder="Enter"
+                                    ref={modalBillRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "bill")}
                                 />
                             </div>
 
@@ -196,6 +356,8 @@ const PurchaseEntry = () => {
                                     value={formData.erc}
                                     onChange={handleChange}
                                     placeholder="Enter"
+                                    ref={modalErcRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "erc")}
                                 />
                             </div>
 
@@ -207,6 +369,8 @@ const PurchaseEntry = () => {
                                     value={formData.frc}
                                     onChange={handleChange}
                                     placeholder="Enter"
+                                    ref={modalFrcRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "frc")}
                                 />
                             </div>
                         </div>
@@ -224,6 +388,8 @@ const PurchaseEntry = () => {
                                 value={formData.value}
                                 onChange={handleChange}
                                 placeholder="Enter"
+                                ref={modalValueRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "value")}
                             />
                         </div>
 
@@ -235,6 +401,8 @@ const PurchaseEntry = () => {
                                 value={formData.disc}
                                 onChange={handleChange}
                                 placeholder="Enter"
+                                ref={modalDiscRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "disc")}
                             />
                         </div>
 
@@ -258,6 +426,8 @@ const PurchaseEntry = () => {
                                 value={formData.percentVat}
                                 onChange={handleChange}
                                 placeholder="Enter"
+                                ref={modalPervatRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "pervat")}
                             />
                         </div>
 
@@ -291,6 +461,8 @@ const PurchaseEntry = () => {
                                 value={formData.purchaseAgst}
                                 onChange={handleChange}
                                 placeholder="Enter"
+                                ref={modalPurchaseagstRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "purchaseagst")}
                             />
                         </div>
 
@@ -302,6 +474,8 @@ const PurchaseEntry = () => {
                                 value={formData.formIssue}
                                 onChange={handleChange}
                                 placeholder="Enter"
+                                ref={modalFormissueRef}
+                                    onKeyDown={(e) => handleKeyNav(e, "formissue")}
                             />
                         </div>
                     </div>
@@ -310,6 +484,8 @@ const PurchaseEntry = () => {
 
             <div className="purchase-button-container">
                 <button 
+                    ref={SubmitRef}
+                    onKeyDown={(e) => handleKeyNav(e, "submit")}
                     onClick={handleSubmit}
                     disabled={loading}
                     className="purchase-submit-button"
