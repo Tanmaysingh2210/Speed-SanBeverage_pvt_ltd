53b969f20d19c93dd280b58b581dfb0fd72384d5
Nihal <chaudharynihal299@gmail.com>
2025-12-16 20:06:41 +0530

    logo

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='nav'>
-      <nav>
+      <nav className="space-between">
+        <div className="san-logo">
+          SAN Beverages
+        </div>
+        <div>
         <ul className='elements'>
             <li><Link to={`/`}>Dashboard</Link></li>
             <li><Link to={`/statistics`}>Statistics</Link></li>
             <li><Link to={`/summary`}>Summary</Link></li>
             
         </ul>
+        </div>
       </nav>
     </div>
   )
