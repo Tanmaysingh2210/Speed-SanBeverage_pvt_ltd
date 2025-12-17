be245976cc1de9c361c13c161002907f21ad03c4
shlok-bhatia <shlokbhatia16@gmail.com>
2025-12-17 12:34:20 +0530

    emt

--- Frontend/src/pages/transaction/LoadOut.jsx
+++ Frontend/src/pages/transaction/LoadOut.jsx
@@ -412,6 +412,7 @@ const LoadOut = () => {
                             </div>
 
                             {items
+                                .filter(itm => itm.container !== "EMT")
                                 .filter(itm =>
                                     itm.code.toLowerCase().includes(search.toLowerCase()) ||
                                     itm.name.toLowerCase().includes(search.toLowerCase())
