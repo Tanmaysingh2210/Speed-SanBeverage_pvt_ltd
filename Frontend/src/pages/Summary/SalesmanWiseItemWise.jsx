ed203346a86dc90450e12b17a947465d7c86508c
shlok-bhatia <shlokbhatia16@gmail.com>
2025-12-24 13:43:18 +0530

    navigations added

--- Frontend/src/pages/Summary/SalesmanWiseItemWise.jsx
+++ Frontend/src/pages/Summary/SalesmanWiseItemWise.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import api from "../../api/api.js";
-import { useState ,useEffect} from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useSalesmanModal } from '../../context/SalesmanModalContext.jsx';
 import "../transaction/transaction.css";
 import { useSalesman } from '../../context/SalesmanContext.jsx';
@@ -12,10 +12,58 @@ const SalesmanWiseItemWise = () => {
   const [rows, setRows] = useState([]);
   const [loading, setLoading] = useState(false);
   const { openSalesmanModal } = useSalesmanModal();
-  const {  getAllSalesmen } = useSalesman();
+  const { getAllSalesmen } = useSalesman();
+  const startRef = useRef(null);
+  const endRef = useRef(null);
+  const findRef = useRef(null);
+  const saleCodeRef = useRef(null);
 
 
-
+  useEffect(() => {
+    startRef.current?.focus();
+  }, []);
+  const handleKeyNav = (e, currentField) => {
+    if (["ArrowRight", "ArrowDown", "Enter"].includes(e.key)) {
+      e.preventDefault();
+      if (e.key === "Enter" && currentField === "find") {
+        findRef.current?.click();
+        return;
+      }
+      switch (currentField) {
+        case "saleCode":
+          startRef.current?.focus();
+          break;
+        case "startDate":
+          endRef.current?.focus();
+          break;
+        case "endDate":
+          if (e.key === "Enter") {
+            findRef.current?.click();
+          } else {
+            findRef.current?.focus();
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    else if (["ArrowUp", "ArrowLeft"].includes(e.key)) {
+      e.preventDefault();
+      switch (currentField) {
+        case "find":
+          endRef.current?.focus();
+          break;
+        case "endDate":
+          startRef.current?.focus();
+          break;
+        case "startDate":
+          saleCodeRef.current?.focus();
+          break;
+        default:
+          break;
+      }
+    }
+  }
 
   const handleFind = async () => {
     if (!salesmanCode || !startDate || !endDate) {
@@ -41,9 +89,9 @@ const SalesmanWiseItemWise = () => {
     }
   };
 
-   useEffect(() => {
-          getAllSalesmen();
-      }, []);
+  useEffect(() => {
+    getAllSalesmen();
+  }, []);
 
   return (
     <div className='trans'>
@@ -53,27 +101,31 @@ const SalesmanWiseItemWise = () => {
             <div className="form-group">
               <label>Salesman Code</label>
               <div className="input-with-btn">
-              <input
-                type="text"
-                value={salesmanCode}
-                onChange={(e) => setSalesmanCode(e.target.value)}
-              />
-              <button
-                type="button"
-                className="dropdown-btn"
-                onClick={() =>
-                  openSalesmanModal((code) =>
-                    setSalesmanCode(code)
-                  )
-                }
-              >
-                ⌄
-              </button>
+                <input
+                onKeyDown={(e) => handleKeyNav(e, "saleCode")}
+                ref={saleCodeRef}
+                  type="text"
+                  value={salesmanCode}
+                  onChange={(e) => setSalesmanCode(e.target.value)}
+                />
+                <button
+                  type="button"
+                  className="dropdown-btn"
+                  onClick={() =>
+                    openSalesmanModal((code) =>
+                      setSalesmanCode(code)
+                    )
+                  }
+                >
+                  ⌄
+                </button>
               </div>
             </div>
             <div className="form-group">
               <label>Start-date</label>
               <input
+                onKeyDown={(e) => handleKeyNav(e, "startDate")}
+                ref={startRef}
                 type="date"
                 value={startDate}
                 onChange={(e) => setStartDate(e.target.value)}
@@ -83,6 +135,8 @@ const SalesmanWiseItemWise = () => {
             <div className="form-group">
               <label>End-date</label>
               <input
+                onKeyDown={(e) => handleKeyNav(e, "endDate")}
+                ref={endRef}
                 type="date"
                 value={endDate}
                 onChange={(e) => setEndDate(e.target.value)}
@@ -90,6 +144,8 @@ const SalesmanWiseItemWise = () => {
             </div>
             <div className="form-group">
               <button onClick={handleFind}
+                ref={findRef}
+                onKeyDown={(e) => handleKeyNav(e, "find")}
                 className="padd trans-submit-btn"
               >
                 {loading ? "Loading..." : "Find"}
