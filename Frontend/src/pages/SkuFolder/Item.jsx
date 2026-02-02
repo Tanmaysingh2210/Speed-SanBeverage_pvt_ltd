8dc37788efd45d319ccc093562b151ce2d2a5944
shlok-bhatia <shlokbhatia16@gmail.com>
2026-02-03 02:25:07 +0530

    pdf/excel added to salesman and depo Master

--- Frontend/src/pages/SkuFolder/Item.jsx
+++ Frontend/src/pages/SkuFolder/Item.jsx
@@ -37,7 +37,7 @@ const Item = () => {
   const [filteredContainers, setFilteredContainers] = useState([]);
   const [filteredFlavours, setFilteredFlavours] = useState([]);
   const [filteredPackages, setFilteredPackages] = useState([]);
-
+  
 
   const [newItem, setNewItem] = useState({
     code: "",
